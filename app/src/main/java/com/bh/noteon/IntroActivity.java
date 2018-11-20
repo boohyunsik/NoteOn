94af675382c9f84913ab16fbddd84ea272ac09fd
Minji Hwang <jjparson93@gmail.com>
2018-11-21 00:22:08 +0900

    2018-11-21 change activity_login.xml change IntroActivity.java

--- app/src/main/java/com/bh/noteon/IntroActivity.java
+++ app/src/main/java/com/bh/noteon/IntroActivity.java
@@ -1,7 +1,10 @@
 package com.bh.noteon;
 
 import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
 
 public class IntroActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class IntroActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable(){
+            @Override
+            public void run(){
+                Intent intent = new Intent(IntroActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
+
     }
 }
