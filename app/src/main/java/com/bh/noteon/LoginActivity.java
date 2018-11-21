13d2cc144d0c75c83250bb2df47be501f2aa7abc
Hyunsik Boo <bhs9194@nate.com>
2018-11-22 00:33:32 +0900

    Add kakao key member values

--- app/src/main/java/com/bh/noteon/LoginActivity.java
+++ app/src/main/java/com/bh/noteon/LoginActivity.java
@@ -17,14 +17,26 @@ public class LoginActivity extends AppCompatActivity {
     private static final String TAG = "LoginActivity";
     // 멤버변수는 mContext 처럼 m으로 시작한다.
     private Context mContext;
+    private String mKeyHash;
+
+    // for Kakako
+    private String mAppKey;
+    private String mRestKey;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // 로그 찍을땐 아래와 같이 Logger 함수를 이용한다.
         Logger.d(TAG, "onCreate()");
 
         mContext = getApplicationContext();
+        mKeyHash = getKeyHash();
+
+        // Kakao key가 필요할 때는 getString 함수를 이용하면 된다.
+        mAppKey = getString(R.string.kakao_native_app_key);
+        mRestKey = getString(R.string.kakao_rest_api_key);
     }
 
     private String getKeyHash() {
@@ -45,6 +57,7 @@ public class LoginActivity extends AppCompatActivity {
                 }
             }
         } catch (PackageManager.NameNotFoundException e) {
+            Logger.e(TAG, e.getMessage());
         }
         return null;
     }
