0019b805b3dc6c046be95b0614c0f4d47886510a
Hyunsik Boo <bhs9194@nate.com>
2018-11-22 00:01:20 +0900

    Update jdk 1.7 to 1.8

--- app/src/main/java/com/bh/noteon/IntroActivity.java
+++ app/src/main/java/com/bh/noteon/IntroActivity.java
@@ -14,14 +14,10 @@ public class IntroActivity extends AppCompatActivity {
         setContentView(R.layout.activity_intro);
 
         Handler handler = new Handler();
-        handler.postDelayed(new Runnable(){
-            @Override
-            public void run(){
-                Intent intent = new Intent(IntroActivity.this, LoginActivity.class);
-                startActivity(intent);
-                finish();
-            }
+        handler.postDelayed(() -> {
+            Intent intent = new Intent(IntroActivity.this, LoginActivity.class);
+            startActivity(intent);
+            finish();
         }, 2000);
-
     }
 }
