307b459de984c1546199de897b22116006b88a3a
Hyunsik Boo <bhs9194@nate.com>
2018-11-22 00:29:42 +0900

    Add getKeyHash method in LoginActivity

--- app/src/main/java/com/bh/noteon/LoginActivity.java
+++ app/src/main/java/com/bh/noteon/LoginActivity.java
@@ -1,13 +1,51 @@
 package com.bh.noteon;
 
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+
+import com.bh.noteon.logger.Logger;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class LoginActivity extends AppCompatActivity {
+    private static final String TAG = "LoginActivity";
+    // 멤버변수는 mContext 처럼 m으로 시작한다.
+    private Context mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Logger.d(TAG, "onCreate()");
+
+        mContext = getApplicationContext();
+    }
+
+    private String getKeyHash() {
+        try {
+            PackageInfo packageInfo = getPackageManager().getPackageInfo(
+                    getPackageName(), PackageManager.GET_SIGNATURES);
+            if (packageInfo == null) {
+                return null;
+            }
+
+            for (Signature signature : packageInfo.signatures) {
+                try {
+                    MessageDigest md = MessageDigest.getInstance("SHA");
+                    md.update(signature.toByteArray());
+                    return Base64.encodeToString(md.digest(), Base64.NO_WRAP);
+                } catch (NoSuchAlgorithmException e) {
+                    Logger.e(TAG, "Unable to get MeesageDigest.");
+                }
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+        }
+        return null;
     }
 }
