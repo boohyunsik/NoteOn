b1fba9264926d97f7de80d89f94c7894976a196c
Minji Hwang <jjparson93@gmail.com>
2018-11-24 01:05:59 +0900

    change KakaoSDKAdapter.java

--- app/src/main/java/com/bh/noteon/LoginActivity.java
+++ app/src/main/java/com/bh/noteon/LoginActivity.java
@@ -75,10 +75,9 @@ public class LoginActivity extends AppCompatActivity {
         @Override
         public void onSessionOpenFailed(KakaoException exception) {
             if(exception != null){
-                Logger.e(exception);
+                Logger.e("KakaoSDKAdapter.java", exception.getMessage());
             }
         }
-
     }
 
     protected void redirectSignupActivity(){
--- app/src/main/java/com/bh/noteon/kakao/KakaoSDKAdapter.java
+++ app/src/main/java/com/bh/noteon/kakao/KakaoSDKAdapter.java
@@ -14,9 +14,15 @@ import com.kakao.auth.AuthType;
 import com.kakao.auth.IApplicationConfig;
 import com.kakao.auth.ISessionConfig;
 import com.kakao.auth.KakaoAdapter;
+import com.kakao.network.ErrorResult;
+import com.kakao.usermgmt.UserManagement;
+import com.kakao.usermgmt.callback.MeV2ResponseCallback;
+import com.kakao.usermgmt.response.MeV2Response;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class KakaoSDKAdapter extends KakaoAdapter {
     private static final String TAG = "KakaoSDKAdapter";
@@ -83,5 +89,38 @@ public class KakaoSDKAdapter extends KakaoAdapter {
                 return GlobalApplication.getGlobalApplicationContext();
             }
         };
+    }
+
+    private void requestMe() {
+        List<String> keys = new ArrayList<>();
+        keys.add("properties.nickname");
+        keys.add("properties.profile_image");
+        keys.add("kakao_account.email");
+//
+//        UserManagement.getInstance().me(keys, new MeV2ResponseCallback() {
+//            @Override
+//            public void onFailure(ErrorResult errorResult) {
+//                String message = "failed to get user info. msg=" + errorResult;
+//                Logger.d("KakakoSDKAdapter.java", message);
+//            }
+
+//            @Override
+//            public void onSessionClosed(ErrorResult errorResult) {
+//                redirectLoginActivity();
+//            }
+//
+//            @Override
+//            public void onSuccess(MeV2Response response) {
+//                Logger.d("KakaoSDKAdapter.java", "user id : " + response.getId());
+//                Logger.d("KakaoSDKAdapter.java", "email: " + response.getKakaoAccount().getEmail());
+//                Logger.d("KakaoSDKAdapter.java", "profile image: " + response.getProfileImagePath());
+//                redirectMainActivity();
+//            }
+//
+//            @Override
+//            public void onNotSignedUp() {
+//                showSignup();
+//            }
+//        });
     }
 }
