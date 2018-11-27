592a60e021ba1f85c6b0e92b485ec082e33716d3
Minji Hwang <jjparson93@gmail.com>
2018-11-27 19:28:08 +0900

    add top navigation bar add menu_top.xml change menu_bottom.xml change MainActivity.java change activity_main.xml

--- app/src/main/java/com/bh/noteon/MainActivity.java
+++ app/src/main/java/com/bh/noteon/MainActivity.java
@@ -25,26 +25,51 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation_view);
+        BottomNavigationView topNavigationView = findViewById(R.id.top_navigation_view);
 
         //첫화면 지정
         FragmentTransaction transaction = fragmentManager.beginTransaction();
         transaction.replace(R.id.frame_layout, menu1Fragment).commitAllowingStateLoss();
 
         //bottomNavigationView의 아이템이 선택될 때 호출될 리스너 등록
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener(){
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener(){
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item){
                 FragmentTransaction transaction = fragmentManager.beginTransaction();
                 switch(item.getItemId()){
-                    case R.id.navigation_menu1: {
+                    case R.id.bottomNavigation_menu1: {
                         transaction.replace(R.id.frame_layout, menu1Fragment).commitAllowingStateLoss();
                         break;
                     }
-                    case R.id.navigation_menu2: {
+                    case R.id.bottomNavigation_menu2: {
                         transaction.replace(R.id.frame_layout, menu2Fragment).commitAllowingStateLoss();
                         break;
                     }
-                    case R.id.navigation_menu3: {
+                    case R.id.bottomNavigation_menu3: {
+                        transaction.replace(R.id.frame_layout, menu3Fragment).commitAllowingStateLoss();
+                        break;
+                    }
+                }
+                return true;
+            }
+        });
+
+        topNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener(){
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item){
+                FragmentTransaction transaction = fragmentManager.beginTransaction();
+                switch(item.getItemId()){
+                    case R.id.topNavigation_menu1: {
+                        transaction.replace(R.id.frame_layout, menu1Fragment).commitAllowingStateLoss();
+                        break;
+                    }
+                    case R.id.topNavigation_menu2: {
+                        transaction.replace(R.id.frame_layout, menu2Fragment).commitAllowingStateLoss();
+                        break;
+                    }
+                    case R.id.topNavigation_menu3: {
                         transaction.replace(R.id.frame_layout, menu3Fragment).commitAllowingStateLoss();
                         break;
                     }
